4eb9c134c3158b4e18a21026575184c047cbab38
changdao <3127955244@qq.com>
2026-04-27 10:20:44 -0700

    fix: datasets get_backbone 接受 f2dc_pgv33 model_name (跟 f2dc_pg 用同 backbone)
    之前只 elif model_name=='f2dc_pg', f2dc_pgv33 fall through 到 else 用普通 resnet10
    导致 v3.3 net(images) 返回单 tensor 而不是 7-tuple, train 时 unpack 报错.
    
    (注: 之前 sc5 跑 v3.3 PACS s=15 时 working dir 含此 fix, sync 时 git checkout 覆盖丢失)

--- F2DC/datasets/pacs.py
+++ F2DC/datasets/pacs.py
@@ -114,7 +114,7 @@ class FedLeaPACS(FederatedDataset):
                 for j in range(parti_num):
                     nets_list.append(resnet10_dc(num_classes=FedLeaPACS.N_CLASS,
                                                  gum_tau=FedLeaPACS.model_args.gum_tau))
-            elif model_name=='f2dc_pg':
+            elif model_name in ('f2dc_pg', 'f2dc_pgv33'):
                 from backbone.ResNet_DC_PG import resnet10_dc_pg
                 args_obj = FedLeaPACS.model_args
                 pw = getattr(args_obj, 'pg_proto_weight', 0.3)
